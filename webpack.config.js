b12d736ce46e77a50871bd097303d8b4e0890a4a
liz <larry.li@dowjoes.com>
2017-12-20 15:30:30 +0800

    Add Chinese locales

--- webpack.config.js
+++ webpack.config.js
@@ -112,7 +112,7 @@ const config = {
       'TALK_PLUGINS_JSON': '{}',
       'TALK_THREADING_LEVEL': '3',
       'TALK_DEFAULT_STREAM_TAB': 'all',
-      'TALK_DEFAULT_LANG': 'en'
+      'TALK_DEFAULT_LANG': 'zh_CN'
     })
   ],
   resolveLoader: {
